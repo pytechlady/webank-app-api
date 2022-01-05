7cf8babd45cce73d18779a46c1194ff6d12e864a
newbietechlady <fumex9910@gmail.com>
2022-01-05 21:46:59 +0100

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0ar3nq(%4&@+q_^-j!&t_f6tzms8t*l#!&q0&40fu3%(7wwn-a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webankapp.herokuapp.com']
 
 
 # Application definition
