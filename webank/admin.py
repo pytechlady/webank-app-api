4e08ec61e9fb7aa4dcc8886817875cee0d8b8ce1
pytechlady <username@Usernames-MacBook-Air.local>
2023-07-10 15:41:29 +0100

    modify admin

--- webank/admin.py
+++ webank/admin.py
@@ -3,15 +3,14 @@ from .models import Balance, User, AccountManager, TransactionHistory
 
 
 # Register your models here.
-admin.site.register(AccountManager)
-admin.site.register(Balance)
-admin.site.register(TransactionHistory)
-
-
-class DisplayAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 
                     'is_verified', 'is_active', 'is_staff', 'updated_at')
     list_display_links = ('id', 'email', 'username')
 
    
-admin.site.register(User, DisplayAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(AccountManager)
+admin.site.register(Balance)
+admin.site.register(TransactionHistory)
+
